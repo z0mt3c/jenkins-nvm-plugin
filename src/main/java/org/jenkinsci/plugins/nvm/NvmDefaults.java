97093d1b19467b015565512317d065dc5e752ef7
Tomas Salazar Leal <atomsmail@gmail.com>
2018-02-21 21:10:49 -0600

    Update NVM default version

--- src/main/java/org/jenkinsci/plugins/nvm/NvmDefaults.java
+++ src/main/java/org/jenkinsci/plugins/nvm/NvmDefaults.java
@@ -2,7 +2,7 @@ package org.jenkinsci.plugins.nvm;
 
 public class NvmDefaults {
 
-  public static String nvmInstallURL = "https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh";
+  public static String nvmInstallURL = "https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh";
   public static String nvmNodeJsOrgMirror = "https://nodejs.org/dist";
   public static String nvmIoJsOrgMirror = "https://iojs.org/dist";
 
